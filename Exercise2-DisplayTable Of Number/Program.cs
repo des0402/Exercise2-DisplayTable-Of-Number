fd35abfec3e27bf44c223a006f7d26fe882f2003
Deepak Sharma <deepak.dev78@outlook.com>
2017-04-27 01:50:25 +1200

    - Display table of the number entered by user

--- Exercise2-DisplayTable Of Number/Program.cs
+++ Exercise2-DisplayTable Of Number/Program.cs
@@ -8,8 +8,22 @@ namespace Exercise2_DisplayTable_Of_Number
 {
     class Program
     {
+        //Write a C# Sharp program that takes a number as input and print its multiplication table
+
         static void Main(string[] args)
         {
+            Console.WriteLine("***Display Table***\n");
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine("{0} x {1} = {2}",number, i, result);
+                
+            }
+
+            Console.ReadLine();
         }
     }
 }
